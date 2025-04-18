2938c934445ef585dd4b3d488c9897b81fdd8b41
wtr <2812542042@qq.com>
2025-04-18 12:13:44 +0800

    修改了uart.c,decode.c,control.c及其对应的头文件使之能与树莓派通信，修改了opencv的串口通信的部分逻辑

--- USER/C/decode.c
+++ USER/C/decode.c
@@ -1,23 +1,23 @@
-#include "usart.h"
+#include "uart.h"
 #include "decode.h"
 
-int flag=0;
 int value=0;
 
-int get_value(){
-
-	flag = USART1_RX_STA ;		
-	
+int rsspi_rx(){	
+	if (start_receive==1){
 	//如果接受到的数据只有1位则开始处理
-	if(flag==1){
-		//清空接收状态
-		USART1_RX_STA = 0;
-		value = str_num-48;
+		if(USART2_RX_STA>=1){
+			//取最新的一个数据位进行处理
+			value = USART2_RX_BUF[USART2_RX_STA-1]-48;
+					//清空接收状态
+			USART2_RX_STA = 0;
+		}
+		//异常处理,在函数调用后要判断返回值是否是INVADE
+		else{
+			return INVADE;		
+		}
+		return value;
 	}
-	//异常处理,在函数调用后要判断返回值是否是INVADE
-	else{
-		return INVADE;
-			
-	}
-	return value;
+	else
+		return 0;
 }
